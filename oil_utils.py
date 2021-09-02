Fixing an issue with oiltrans, adding water do density computation

oil_utils.py
@@ -6,6 +6,7 @@ Created on Thu Mar 18 13:20:25 2021
 """
 
 import math
+import numpy as np
 
 def interp(val, array_value, array_ref):
     """
@@ -217,17 +218,20 @@ class mix:
 
     def add_amount(self, add_amount):
         """
-        This function add/remove for each component at the same time
+        This function add/remove for each component at the same time and return
+        an array with the quantities added to each component
         """
 
         tot_amount = 0
-
+        array_tr = np.zeros((len(self.list_component)))
         for comp in self.list_component:
             tot_amount += comp.amount
 
-        for comp in self.list_component:
-            comp.amount += add_amount * (comp.amount/tot_amount)
-
+        for i in range(0, len(self.list_component)):
+            amount = add_amount * (self.list_component[i].amount/tot_amount)
+            self.list_component[i].amount += amount
+            array_tr[i] = amount
+        return array_tr
 
 
     def get_density(self, T):
@@ -243,8 +247,24 @@ class mix:
         return interp(T, self.viscosity, self.viscosity_T)
 
 
+    def get_emulsion_density(self, T, array_in_emulsion):
+        """
+        Return the density by taking into account each component, T is the temperature,
+        array_in_emulsion is an array with the amount of each component in emulsion.
+        """
+        water_volume = 0
+        water_density = 1020
+        oil_volume = 0
+        oil_mass = 0
+        for i in range(0, len(self.list_component)):
+            component_density = self.list_component[i].get_density(T)
+            if component_density is None :
+                return None
+            oil_volume += (self.list_component[i].amount+array_in_emulsion[i])
+            oil_mass += (self.list_component[i].amount+array_in_emulsion[i]) * component_density
+            water_volume += array_in_emulsion[i] *(self.max_water/(1-self.max_water))
 
-
+        return (oil_mass+water_volume*1020)/(oil_volume+water_volume)
 
 
 class component:

weathering_utils.py
@@ -87,7 +87,7 @@ def plot_matrix_mix(mix, matrix, percent = False):
         plt.plot(matrix[:,0]/3600,matrix[:,4]/sum*100)
         plt.plot(matrix[:,0]/3600,matrix[:,5]/sum*100)
         plt.plot(matrix[:,0]/3600,matrix[:,6]/sum*100)
-        plt.ylabel('%')
+        plt.ylabel('% (Volume)')
         plt.ylim([0, matrix[0,1]]/sum*100)
     else :
         plt.plot(matrix[:,0]/3600,matrix[:,1])
@@ -147,6 +147,8 @@ def all_process(mix, temperature, wind_speed, sim_length, dt, water_volume,
 
     """
 
+    array_in_emulsion = np.zeros((len(mix.list_component)))
+
     time_step_amount = int(sim_length / dt)
     matrix = np.zeros((int(time_step_amount),8))
 
@@ -179,7 +181,7 @@ def all_process(mix, temperature, wind_speed, sim_length, dt, water_volume,
                 comp_area = comp.amount / matrix[i-1,1] * area
                 if comp.amount > 0 :
                     fract = mix.get_molar_fract(comp)
-                    if comp.boiling_T < 1000:
+                    if comp.boiling_T < 1000 and ev.find_vapor_pressure(comp, temperature) > 0:
                     #evaporation pseudo component
                         flux = 0
                         #OILTRANS
@@ -189,7 +191,7 @@ def all_process(mix, temperature, wind_speed, sim_length, dt, water_volume,
                             #k from mw
                             k_evp = ev.mass_transfer_coefficient_OILTRANS(wind_speed, length,
                                                   schmdt_nmbr_air)
-                            flux = ev.evap_volume_OILTRANS(k_evp, comp_area, p_oil,
+                            flux = ev.evap_volume_OILTRANS(k_evp, area, p_oil,
                                                           molar_v,temperature,
                                                           molar_fraction = fract)
                         #ALOHA
@@ -204,7 +206,8 @@ def all_process(mix, temperature, wind_speed, sim_length, dt, water_volume,
                             flux = 0
                             if comp.partial_P is not None:
                                 cs = ev.vapor_phase_sat_conc(comp.molar_weight, comp.partial_P,
-                                                          temperature)
+                                                              ev.find_vapor_pressure(comp, temperature),
+                                                              temperature)
                                 flux = (ev.evap_mass_flux_ALOHA(cs, wind_friction, k)
                                         * comp_area / comp.density)
                         #print(comp.amount)
@@ -294,7 +297,7 @@ def all_process(mix, temperature, wind_speed, sim_length, dt, water_volume,
                             * ( matrix[i-1,6] +  mix.get_prop(temperature).amount))
                     if mix.get_prop(temperature).amount < flux:
                         flux = mix.get_prop(temperature).amount
-                    mix.add_amount(-flux)
+                    array_in_emulsion -= mix.add_amount(-flux)
                     emul_fl += flux
         prop = mix.get_prop(temperature)
         matrix[i,1] = prop.amount
@@ -303,7 +306,7 @@ def all_process(mix, temperature, wind_speed, sim_length, dt, water_volume,
         matrix[i,4] = bio_fl  + matrix[i-1,4]
         matrix[i,5] = phot_fl  + matrix[i-1,5]
         matrix[i,6] = emul_fl  + matrix[i-1,6]
-        matrix[i,7] = prop.density
+        matrix[i,7] = mix.get_emulsion_density(temperature,array_in_emulsion)
     return matrix
 
 